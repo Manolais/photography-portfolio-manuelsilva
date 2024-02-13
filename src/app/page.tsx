a3424848a25eb7384a21518eaa19d1eee0fbad1c
Manuel <bsasmanuelsilva@gmail.com>
2024-02-13 07:58:35 -0300

    Move resume.txs and removed unnecessary imports

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,14 +3,7 @@ import { Navbar, Footer } from "@/components";
 
 // sections
 import Hero from "./hero";
-import Clients from "./clients";
-import Skills from "./skills";
-import Projects from "./projects";
-import Resume from "./resume";
-import Testimonial from "./testimonial";
-import PopularClients from "./popular-clients";
 import ContactForm from "./contact-form";
-import FeaturedImageGallery from "./featured-galery";
 import MasonryGridGallery from "./mansory-grid-galery";
 
 export default function Portfolio() {
--- src/app/servicios/page.tsx
+++ src/app/servicios/page.tsx
@@ -1,14 +1,5 @@
 import { Navbar, Footer } from "@/components";
-import Hero from "../hero";
-import Clients from "../clients";
-import Skills from "../skills";
-import Projects from "../projects";
-import Resume from "../resume";
-import Testimonial from "../testimonial";
-import PopularClients from "../popular-clients";
-import ContactForm from "../contact-form";
-import FeaturedImageGallery from "../featured-galery";
-import MasonryGridGallery from "../mansory-grid-galery";
+import Resume from "./resume";
 
 //sections
 export default function Page() {
